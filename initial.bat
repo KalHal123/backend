abe5955da8bf661c41eb27bca07bdcd34e5dc9d6
KalHal123 <kh7784709@gmail.com>
2024-12-05 20:18:07 +0100

    Update

--- initial.bat
+++
@@ -1,7 +0,0 @@
-echo "# backend" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/KalHal123/backend.git
-git push -u origin main
